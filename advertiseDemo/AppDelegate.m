10be77f2c213ca867dbe7ebbc37202d176705520
周焕强 <461416254@qq.com>
2016-06-06 13:30:30 +0800

    fix #1

--- advertiseDemo/AppDelegate.m
+++ advertiseDemo/AppDelegate.m
@@ -63,39 +63,24 @@
  */
 - (void)getAdvertisingImage
 {
-    CGFloat scale_screen = [UIScreen mainScreen].scale;
-    NSLog(@"scale :%.0f, %.0f    %.0f",scale_screen,kscreenWidth*scale_screen,kscreenHeight*scale_screen);
-    int scaleW = (int)kscreenWidth*scale_screen;
-    int scaleH = (int)kscreenHeight*scale_screen;
-    // 这里采用了美团的广告接口
-    NSString *urlStr = [NSString stringWithFormat:@"http://api.meituan.com/config/v1/loading/check.json?app_name=group&app_version=5.7&ci=1&city_id=1&client=iphone&movieBundleVersion=100&msid=48E2B810-805D-4821-9CDD-D5C9E01BC98A2015-07-15-15-51824&platform=iphone&resolution=%d%@%d&userid=10086&utm_campaign=AgroupBgroupD100Fa20141120nanning__m1__leftflow___ab_pindaochangsha__a__leftflow___ab_gxtest__gd__leftflow___ab_gxhceshi__nostrategy__leftflow___ab_i550poi_ktv__d__j___ab_chunceshishuju__a__a___ab_gxh_82__nostrategy__leftflow___ab_i_group_5_3_poidetaildeallist__a__b___b1junglehomepagecatesort__b__leftflow___ab_gxhceshi0202__b__a___ab_pindaoquxincelue0630__b__b1___ab_i550poi_xxyl__b__leftflow___ab_i_group_5_6_searchkuang__a__leftflow___i_group_5_2_deallist_poitype__d__d___ab_pindaoshenyang__a__leftflow___ab_b_food_57_purepoilist_extinfo__a__a___ab_waimaiwending__a__a___ab_waimaizhanshi__b__b1___ab_i550poi_lr__d__leftflow___ab_i_group_5_5_onsite__b__b___ab_xinkeceshi__b__leftflowGhomepage&utm_content=4B8C0B46F5B0527D55EA292904FD7E12E48FB7BEA8DF50BFE7828AF7F20BB08D&utm_medium=iphone&utm_source=AppStore&utm_term=5.7&uuid=4B8C0B46F5B0527D55EA292904FD7E12E48FB7BEA8DF50BFE7828AF7F20BB08D&version_name=5.7",scaleW,@"%2A",scaleH];
+
+    // 这里原本采用美团的广告接口，现在没法用了。所以用了一些固定的图片url代替
+    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/fd039245d688d43f70b37c1a7d1ed21b0ff43bc4.jpg", @"http://a4.mzstatic.com/us/r30/Purple6/v4/93/3e/10/933e1072-791b-49d5-4292-db6cb4f7ab5f/screen640x960.jpeg", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://a.hiphotos.baidu.com/image/pic/item/b21c8701a18b87d6ff2ca7bc030828381f30fd23.jpg"];
+    NSString *imageUrl = imageArray[arc4random() % imageArray.count];
     
-    AFHTTPRequestOperationManager * manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/html", nil];
-    [manager GET:urlStr parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
-        NSLog(@"%@", responseObject);
-        NSArray *dataArray = responseObject[@"data"];
-        NSString *imageUrl = nil;
-        if (dataArray.count) {
-           imageUrl = dataArray[0][@"imageUrl"];
-        }else{
-            imageUrl = @"http://img1.gamedog.cn/2013/08/05/43-130P5122Z60-50.jpg";
-        }
-        
- 
-        NSArray *stringArr = [imageUrl componentsSeparatedByString:@"/"];
-        NSString *imageName = stringArr.lastObject;
-        NSString *filePath = [self getFilePathWithImageName:imageName];
-        BOOL isExist = [self isFileExistWithFilePath:filePath];
-        if (!isExist){// 如果该图片不存在，则删除老图片，下载新图片
+    // 获取图片名:43-130P5122Z60-50.jpg
+    NSArray *stringArr = [imageUrl componentsSeparatedByString:@"/"];
+    NSString *imageName = stringArr.lastObject;
+    
+    // 拼接沙盒路径
+    NSString *filePath = [self getFilePathWithImageName:imageName];
+    BOOL isExist = [self isFileExistWithFilePath:filePath];
+    if (!isExist){// 如果该图片不存在，则删除老图片，下载新图片
             
-            [self downloadAdImageWithUrl:imageUrl imageName:imageName];
-            
-        }
+        [self downloadAdImageWithUrl:imageUrl imageName:imageName];
         
-    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
-        
-    }];
+    }
+
 }
 
 /**
