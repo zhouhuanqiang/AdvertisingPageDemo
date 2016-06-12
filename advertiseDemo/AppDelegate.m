ea836074829691db81f480af8787803ef06ba84d
zhouhuanqiang <461416254@qq.com>
2016-06-12 20:36:04 +0800

    修改广告图url

--- advertiseDemo/AppDelegate.m
+++ advertiseDemo/AppDelegate.m
@@ -63,8 +63,10 @@
 - (void)getAdvertisingImage
 {
 
-    // 这里原本采用美团的广告接口，现在好像没法用了。所以用了一些固定的图片url代替
-    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/fd039245d688d43f70b37c1a7d1ed21b0ff43bc4.jpg", @"http://a4.mzstatic.com/us/r30/Purple6/v4/93/3e/10/933e1072-791b-49d5-4292-db6cb4f7ab5f/screen640x960.jpeg", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://a.hiphotos.baidu.com/image/pic/item/b21c8701a18b87d6ff2ca7bc030828381f30fd23.jpg"];
+    // TODO 请求广告接口
+    
+    // 这里原本采用美团的广告接口，现在了一些固定的图片url代替
+    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/9213b07eca80653846dc8fab97dda144ad348257.jpg", @"http://pic.paopaoche.net/up/2012-2/20122220201612322865.png", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://www.mangowed.com/uploads/allimg/130410/1-130410215449417.jpg"];
     NSString *imageUrl = imageArray[arc4random() % imageArray.count];
     
     // 获取图片名:43-130P5122Z60-50.jpg
